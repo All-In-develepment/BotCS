4c281fdb256a16262d67263b69ab221bc779a5ac
ronaldocestrela <132525855+ronaldocestrela@users.noreply.github.com>
2024-05-10 13:50:45 -0300

    Check de resultados

--- Results/checkResult.py
+++ Results/checkResult.py
@@ -1,12 +1,27 @@
-import requests
+from Games import getMatchByMatchId
+from Messages.messagens import SendMessage
 
 
-def CheckResult(match_id):
-    try:
-        match = requests.get(f'http://191.252.5.229:8080/api/Tips/match/{match_id}').json()
-        if match['status']:
-            return match
-        else:
-            return False
-    except:
-        return False
+def CheckResult(tip):
+    point = 0
+    match = getMatchByMatchId(tip["tipMatchId"])
+    print(match)
+    for entrada in tip["tipMapOdd"]:
+        try:
+            entrada_float = entrada.replace(",", ".")
+            entrada_float = float(entrada_float)
+            print(entrada_float)
+        except:
+            print("Não tem entrada para esse mapa")
+            entrada_float = "Não tem entrada para esse mapa"
+            
+        if ("winnerTeam" in match):
+            maps_rouds = match["maps"][point]["result"]["team1TotalRounds"] + match["maps"][point]["result"]["team2TotalRounds"]
+        
+        if entrada_float != "Não tem entrada para esse mapa":
+            if maps_rouds < entrada_float:
+                print("Ganhou")
+                SendMessage("Ganhou")
+            else:
+                print("Perdeu")
+            point += 1
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from Games import getAllGames, filterNextGames, getMapsBO1, getMapsBO3, getMapsBO5, getMatchByMatchId, getMatchInfo, strutuctInfo
 from Messages.messagens import SendMessage
+from Results.checkResult import CheckResult
 from Tips.TipsManager import VerifyOpenTip
 from verifyerTeam import AvgWinPeerLoos, FavoriteTeam
 from DataBases.Connection import GetOpenTips, InsertOne
@@ -180,42 +181,43 @@ while True:
     try:
         match_in_db = GetOpenTips()
         
-        point = 0
+        # point = 0
         for tip in match_in_db:
-            match = getMatchByMatchId(tip["tipMatchId"])
-            print(tip["tipMatchId"])
-            for entrada in tip["tipMapOdd"]:
-                try:
-                    entrada_float = entrada.replace(",", ".")
-                    entrada_float = float(entrada_float)
-                    print(entrada_float)
-                except:
-                    print("Não tem entrada para esse mapa")
-                    entrada_float = "Não tem entrada para esse mapa"
+            CheckResult(tip)
+            # match = getMatchByMatchId(tip["tipMatchId"])
+            # print(tip["tipMatchId"])
+            # for entrada in tip["tipMapOdd"]:
+            #     try:
+            #         entrada_float = entrada.replace(",", ".")
+            #         entrada_float = float(entrada_float)
+            #         print(entrada_float)
+            #     except:
+            #         print("Não tem entrada para esse mapa")
+            #         entrada_float = "Não tem entrada para esse mapa"
                     
-                if ("winnerTeam" in match):
-                    maps_rouds = match["maps"][point]["result"]["team1TotalRounds"] + match["maps"][point]["result"]["team2TotalRounds"]
+            #     if ("winnerTeam" in match):
+            #         maps_rouds = match["maps"][point]["result"]["team1TotalRounds"] + match["maps"][point]["result"]["team2TotalRounds"]
                 
-                if entrada_float != "Não tem entrada para esse mapa":
-                    if maps_rouds < entrada_float:
-                        print("Ganhou")
-                        # with open('Messages/entrada_message.txt', 'r', encoding='utf-8') as file:
-                        #             entrada = file.read().format(
-                        #                 time_a=match['team1']['name'],
-                        #                 time_b=match['team2']['name'],
-                        #                 match_date=date_timestemp.strftime('%d/%m/%Y'),
-                        #                 match_time=date_timestemp.strftime('%H:%M'),
-                        #                 favorite_team=favorite_team,
-                        #                 map_1=informacoes['firt_map_element'],
-                        #                 map_2=informacoes['second_map_element'],
-                        #                 map_3=informacoes['third_map_element'],
-                        #                 entrada_1=f'UNDER {first_map_tip}',
-                        #                 entrada_2=f'UNDER {second_map_tip}',
-                        #                 entrada_3=f'UNDER {third_map_tip}'
-                        #             )
-                    else:
-                        print("Perdeu")
-                    point += 1
+            #     if entrada_float != "Não tem entrada para esse mapa":
+            #         if maps_rouds < entrada_float:
+            #             print("Ganhou")
+            #             # with open('Messages/entrada_message.txt', 'r', encoding='utf-8') as file:
+            #             #             entrada = file.read().format(
+            #             #                 time_a=match['team1']['name'],
+            #             #                 time_b=match['team2']['name'],
+            #             #                 match_date=date_timestemp.strftime('%d/%m/%Y'),
+            #             #                 match_time=date_timestemp.strftime('%H:%M'),
+            #             #                 favorite_team=favorite_team,
+            #             #                 map_1=informacoes['firt_map_element'],
+            #             #                 map_2=informacoes['second_map_element'],
+            #             #                 map_3=informacoes['third_map_element'],
+            #             #                 entrada_1=f'UNDER {first_map_tip}',
+            #             #                 entrada_2=f'UNDER {second_map_tip}',
+            #             #                 entrada_3=f'UNDER {third_map_tip}'
+            #             #             )
+            #         else:
+            #             print("Perdeu")
+                    # point += 1
     except Exception as e:
         print(e)
     
