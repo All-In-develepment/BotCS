5f45d7674d4403be01817b6264c07a58db3fd9dc
ronaldocestrela <132525855+ronaldocestrela@users.noreply.github.com>
2024-05-10 14:25:39 -0300

    Adição do botão de evento

--- Messages/messagens.py
+++ Messages/messagens.py
@@ -16,3 +16,18 @@ def SendMessage(message):
     print('Mensagem enviada com sucesso')
     
     return msg_id.message_id
+
+def EditMessage(message_id, message):
+    # Substitua 'YOUR_BOT_TOKEN' pelo token do seu bot
+    bot_token = '5913009769:AAGXYwrzoEUK9KHXuxdpxtbPXOEhX3ElOfo'
+    # Substitua 'CHANNEL_ID' pelo ID do canal onde você deseja enviar a mensagem
+    channel_id = '-1002144207029'
+
+    # Crie uma instância do bot
+    bot = telebot.TeleBot(bot_token)
+
+    # Envie a mensagem para o canal
+    msg_id = bot.send_message(channel_id, message)
+
+    # Imprima uma mensagem de sucesso
+    print('Mensagem enviada com sucesso')
