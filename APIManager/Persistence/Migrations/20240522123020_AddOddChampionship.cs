fb22872232c50104b34fcb97f4ad6437c3552e99
ronaldocestrela <132525855+ronaldocestrela@users.noreply.github.com>
2024-05-22 09:37:24 -0300

    Adição de campeonato

---
+++ APIManager/Persistence/Migrations/20240522123020_AddOddChampionship.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOddChampionship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TipChampionship",
+                table: "Tips",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipsMapResultOdd",
+                table: "Tips",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipChampionship",
+                table: "Tips");
+
+            migrationBuilder.DropColumn(
+                name: "TipsMapResultOdd",
+                table: "Tips");
+        }
+    }
+}
